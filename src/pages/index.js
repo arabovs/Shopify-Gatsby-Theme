040a67d044ba072ba8ef846e988988da5317c193
Simeon Arabov <s.arabov@myxrobotics.com>
2023-11-09 03:19:13 +0200

    redesign index

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,136 +72,107 @@ const IndexPage = ({ data }) => {
 
   return (
     <Box>
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          justifyItems: "center",
-        }}
-      >
-        <Grid item sx={{ flex: "1" }}>
-          asd
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Box>
-            <Box textAlign="center">
-              <Box
-                py={4}
-                padding={4}
-                textAlign="center"
-                sx={{ position: "relative" }}
+      <Box>
+        <Box textAlign="center">
+          <Box
+            py={4}
+            padding={4}
+            textAlign="center"
+            sx={{ position: "relative" }}
+          >
+            <Typography
+              color="white"
+              gutterBottom
+              style={{
+                marginTop: 4,
+                fontFamily: "Great Vibes",
+                position: "absolute",
+                top: "20",
+                left: "50%",
+                transform: "translateX(-50%)",
+                width: "80%",
+                zIndex: 1,
+                color: "#4f4759",
+                fontStyle: "bold",
+                fontSize: fontSize,
+              }}
+            >
+              Welcome to The Art in Lounge
+              <br />
+              Showcasing Women in Art
+            </Typography>
+
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "relative",
+              }}
+            >
+              <StaticImage
+                src="../images/banner.jpg"
+                alt="Banner"
+                layout="constrained"
+                sx={{
+                  width: "100%",
+                }}
+              />
+            </Box>
+            <Paper
+              elevation={4}
+              sx={{
+                marginTop: 4,
+                backgroundColor: "#8B7D9B",
+              }}
+            >
+              <Link
+                to="/products"
+                style={{
+                  textDecoration: "none",
+                  fontFamily: "Great Vibes",
+                }}
+                sx={{}}
               >
                 <Typography
-                  color="white"
-                  gutterBottom
-                  style={{
-                    marginTop: 4,
-                    fontFamily: "Great Vibes",
-                    position: "absolute",
-                    top: "20",
-                    left: "50%",
-                    transform: "translateX(-50%)",
-                    width: "80%",
-                    zIndex: 1,
-                    color: "#4f4759",
-                    fontStyle: "bold",
-                    fontSize: fontSize,
-                  }}
-                >
-                  Welcome to The Art in Lounge
-                  <br />
-                  Showcasing Women in Art
-                </Typography>
-
-                <Box
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    position: "relative",
-                  }}
-                >
-                  <StaticImage
-                    src="../images/banner.jpg"
-                    alt="Banner"
-                    layout="constrained"
-                    sx={{
-                      width: "100%",
-                    }}
-                  />
-                </Box>
-                <Paper
-                  elevation={4}
                   sx={{
-                    marginTop: 4,
-                    backgroundColor: "#8B7D9B",
+                    fontFamily: "Great Vibes",
+                    color: "white",
+                    lineHeight: "1.8",
                   }}
+                  variant="h3"
+                  textAlign="center"
                 >
-                  <Link
-                    to="/products"
-                    style={{
-                      textDecoration: "none",
-                      fontFamily: "Great Vibes",
-                    }}
-                    sx={{}}
-                  >
-                    <Typography
-                      sx={{
-                        fontFamily: "Great Vibes",
-                        color: "white",
-                        lineHeight: "1.8",
-                      }}
-                      variant="h3"
-                      textAlign="center"
-                    >
-                      Shop Now
-                    </Typography>
-                  </Link>
-                </Paper>
-              </Box>
-
-              {!isSmallScreen && (
-                <Typography
-                  style={{
-                    fontFamily: "Playfair Display, serif",
-                    fontSize: 22,
-                  }}
-                  variant="body1"
-                  paragraph
-                  sx={{ marginTop: 2, padding: 2 }}
-                >
-                  Dear you, Welcome to Art in Lounge! Your fashion soulmate, and
-                  place of artistry and freedom. At our establishment we will
-                  greet you with classical elegance to bold colours. Each piece
-                  of clothing is meticulously designed to reflect your unique
-                  style and preferences, escaping from the fast fashion trends,
-                  and ensuring you step into your evenings with an aura of
-                  sensuality. Expression of character through clothing is a
-                  powerful tool. And we will help you use it! Welcome to your
-                  exquisite night lounge clothing partner, nestled in the heart
-                  of Bulgaria, where we craft the epitome of artistry and
-                  comfort.
+                  Shop Now
                 </Typography>
-              )}
-            </Box>
+              </Link>
+            </Paper>
           </Box>
-          <Box>{/* Content for the second middle box */}</Box>
-        </Grid>
-        <Grid item sx={{ flex: "1", height: "100%", width: "100%" }}>
-          asd
-        </Grid>
-      </Grid>
+
+          {!isSmallScreen && (
+            <Typography
+              style={{
+                fontFamily: "Playfair Display, serif",
+                fontSize: 22,
+              }}
+              variant="body1"
+              paragraph
+              sx={{ marginTop: 2, padding: 2 }}
+            >
+              Dear you, Welcome to Art in Lounge! Your fashion soulmate, and
+              place of artistry and freedom. At our establishment we will greet
+              you with classical elegance to bold colours. Each piece of
+              clothing is meticulously designed to reflect your unique style and
+              preferences, escaping from the fast fashion trends, and ensuring
+              you step into your evenings with an aura of sensuality. Expression
+              of character through clothing is a powerful tool. And we will help
+              you use it! Welcome to your exquisite night lounge clothing
+              partner, nestled in the heart of Bulgaria, where we craft the
+              epitome of artistry and comfort.
+            </Typography>
+          )}
+        </Box>
+      </Box>
 
       <Container>
         <Seo title="Home" />
